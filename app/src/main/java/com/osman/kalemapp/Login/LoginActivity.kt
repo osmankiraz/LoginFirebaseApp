fc3d64c1606158e105c9d02cced2d0073067f225
Osman Kiraz <45825918+osmnkiraz@users.noreply.github.com>
2021-06-15 22:19:25 +0300

    initial commit

---
+++ app/src/main/java/com/osman/kalemapp/Login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.osman.kalemapp.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import com.osman.kalemapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
---
+++ app/src/main/java/com/osman/kalemapp/Login/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.osman.kalemapp.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.osman.kalemapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
